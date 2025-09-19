1b214399bf7cd6bb13a2d8f329fca30c880a90f1
willbo <willbfo@gmail.com>
2025-09-19 09:26:28 +0200

    test merge
    testar merge

--- Assets/Scripts/PlayMovement.cs
+++ Assets/Scripts/PlayMovement.cs
@@ -11,7 +11,7 @@ public class PlayMovement : MonoBehaviour
     Rigidbody2D rd2D;
 
     private bool betterJump = false;
-   
+   //test test igen från playermovement för att få merge konflikt
     public float fallMultipiler = 0.5f;
     public float lowJumpMultiplier = 1f;
 
